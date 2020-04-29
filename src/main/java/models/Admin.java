fe8769dd2b7ba4d434d0efc117802d633fdb3b3d
Bahar <asadibahar244@gmail.com>
2020-04-28 18:37:32 -0700

    First Model Commit

---
+++ src/main/java/models/Admin.java
@@ -0,0 +1,17 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Admin extends Person{
+    private ArrayList<Request> allRequests;
+
+    public Admin(HashMap<String, String> information) {
+        super(information);
+        this.allRequests = new ArrayList<>();
+    }
+
+    public ArrayList<Request> getAllRequests() {
+        return allRequests;
+    }
+}
